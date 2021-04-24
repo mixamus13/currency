292cff8778d395c60c6d5126b0ea33b17461b57d
Maxim Dokuchaev <MDokuchaev@luxoft.com>
2021-04-24 12:40:25 +0300

    added-databaseh2

--- src/test/java/com/mixamus/currency/controller/CurrencyControllerTest.java
+++ src/test/java/com/mixamus/currency/controller/CurrencyControllerTest.java
@@ -1,13 +1,29 @@
 package com.mixamus.currency.controller;
 
+import com.mixamus.currency.model.Currency;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import sun.jvm.hotspot.HelloWorld;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(Currency.class)
 class CurrencyControllerTest {
 
-    @Test
-    void getCurrencyByDate() {
+    @Autowired
+    private MockMvc mockMvc;
 
+    @Test
+    void getCurrencyByDate() throws Exception {
+
+        this.mockMvc.perform(get("/")
+                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
     }
 }
