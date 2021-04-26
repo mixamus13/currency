91c982d0730745d49e1a9b205fd7b8a13c138b0d
Maxim Dokuchaev <MDokuchaev@luxoft.com>
2021-04-26 11:03:47 +0300

    controller-test-okay

--- src/test/java/com/mixamus/currency/controller/CurrencyControllerTest.java
+++ src/test/java/com/mixamus/currency/controller/CurrencyControllerTest.java
@@ -5,10 +5,13 @@ import com.mixamus.currency.service.CurrencyService;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,15 +20,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDate;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-@WebMvcTest(controllers = CurrencyController.class)
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest
+@DisplayName("Currency controller test by data")
 class CurrencyControllerTest {
-
-    @InjectMocks
-    private CurrencyController currencyController;
 
     @Autowired
     private MockMvc mvc;
@@ -35,9 +33,9 @@ class CurrencyControllerTest {
 
     @Test
     @DisplayName("Should Currency when making GET request to endpoint /currencydate")
-    void shouldCurrencyByData() throws Exception {
+    public void shouldCurrencyByData() throws Exception {
 
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2021, 7, 12);
         Currency currency = new Currency(localDate, "USD", 98.34);
 
         Mockito.when(currencyService.getCurrencyByDate(localDate))
@@ -46,20 +44,16 @@ class CurrencyControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/currencydate/?data=2021-07-12"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(1)));
-    }
-
-    @Test
-    @DisplayName("Return status is OK")
-    public void shouldReturnIsOk() throws Exception {
-        this.mvc.perform(get("/currencydate/?data=2021-07-12"))
-                .andExpect(status().isOk());
+                .andExpect(MockMvcResultMatchers.jsonPath("$.size()", Matchers.is(3)))
+                .andExpect(MockMvcResultMatchers.jsonPath("rate", Matchers.is(98.34)))
+                .andExpect(MockMvcResultMatchers.jsonPath("code", Matchers.is("USD")));
     }
 
     @Test
     @DisplayName("Current controller is not null")
-    public void getCurrencyControllerNotNull() {
-        assertThat(currencyController)
-                .isNotNull();
+    public void getCurrencyControllerNotNull() throws Exception {
+
+        mvc.perform(MockMvcRequestBuilders.get("/currencydate?data="))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
